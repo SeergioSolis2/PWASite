cbfe177a2509407fca4b63d559d1d732b6be9eae
Sergio Solis <sergiosolishdz30@gmail.com>
2022-05-27 14:21:02 -0500

    primer comentario

---
+++ src/assets/script.js
@@ -0,0 +1,6 @@
+if('serviceWorker'in navigator){
+    navigator.serviceWorker.register('./assets/sw.js')
+    .then(reg=>console.log('Registro SW exitoso',reg))
+    .catch(err=>console.warn('error al tratar de registrar el sw',err))
+}
+
