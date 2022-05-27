5a90eab48ee1e60d4babbb2c915648478f3b4870
SeergioSolis2 <85816568+SeergioSolis2@users.noreply.github.com>
2022-05-27 14:52:26 -0500

    Yeah

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,5 +1,5 @@
 if('serviceWorker'in navigator){
-    navigator.serviceWorker.register('./assets/sw.js')
+    navigator.serviceWorker.register('/src/assets/sw.js')
     .then(reg=>console.log('Registro SW exitoso',reg))
     .catch(err=>console.warn('error al tratar de registrar el sw',err))
 }
